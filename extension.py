5a23e6f64ee016e7d5aaa0b9b03d7ed1cd4c01a0
mattbachmann <matth.bachmann@gmail.com>
2021-09-11 21:48:16 +0200

    Papa

--- extension.py
+++ extension.py
@@ -1,23 +1,33 @@
-from abc import ABCMeta
+from abc import ABC
 from abc import abstractmethod
 
-class Xformation(metaclass=ABCMeta):
+class Papa(ABC):
+    cool = "Yea!"
+
+    def __init__(self, name: str):
+        self.name = name
+
     @abstractmethod
     def apply(self, input: str) -> str:
         pass
 
-class AdjectiveX(Xformation):
-    def __init__(self, adjective: str):
-        self.adjective = adjective
-
-    def apply(self, input: str) -> str:
-        return f"{self.adjective} {input}"
+class Bobo(Papa):
+     def apply(self, input):
+        print(Bobo.cool)
+        return self.name + " " + input
+   
+# test = Xformation()
 
 nouns = ["Winnie", "Dumbo", "Igeli"]
-xform = AdjectiveX("super");
+
+xform = Bobo("super")
+
+for x in nouns:
+    print(x + " -> " + xform.apply(x))
+
 result = list(map(xform.apply, nouns))
 print(result)
 
-result2 = list(map(AdjectiveX("mega").apply, result))
+result2 = list(map(Bobo("mega").apply, result))
 print(result2)
 
